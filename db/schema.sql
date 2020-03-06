c62d22436e34c7639da2dc275832679e9426914d
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-03-05 21:06:47 -0800

    added columsn to schema for measurement size

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,9 @@ USE recipes_db;
 CREATE TABLE recipes (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    ingredients VARCHAR(500) NOT NULL,
+    amount INT NOT NULL,
+    measurement VARCHAR(30) NOT NULL,
+    ingredientname VARCHAR(500) NOT NULL,
     category VARCHAR(50) NOT NULL,
     servingsize INT,
     PRIMARY KEY (id) 
