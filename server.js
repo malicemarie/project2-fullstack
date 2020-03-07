561f7e02b8107fb5a556a1e42805d1e28513f9d4
Esther Walker <equestriennesrule@yahoo.com>
2020-03-06 19:14:12 -0800

    added a recipe.js file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(`public`));
 
 require(`./routes/api-routes.js`)(app);
-require(`./routes/html-routes.js`)(app);
+//require(`./routes/html-routes.js`)(app);
 
 db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () =>
