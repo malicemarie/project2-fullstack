17db1f41570ad7254ec8116d65ea6f88dbca6861
Spencer Shadley <spencershadley@utexas.edu>
2020-03-10 19:08:14 -0700

    fixed stuff

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,10 +2,10 @@
 
 module.exports = app => {
   app.get(`/`, (req, res) => {
-    res.render("index", { layout: "index" });
+    res.render("index");
   });
 
-  app.get(`/api/recipes`, (req, res) => {
-    res.render("viewall", { layout: "viewall" });
+  app.get(`/recipes`, (req, res) => {
+    res.render("viewall", { recipes: [{ title: 'pizza', ingredientname: 'cheese', category: 'food' }] });
   });
 };
