867246f488a2ec4cdfb021391fe7b9bf8a07369b
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-03-10 18:28:22 -0700

    updated html and server file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,6 @@
 "use strict";
 
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-const path = require(`path`);
-
-// Routes
-// =============================================================
 module.exports = app => {
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
   app.get(`/`, (req, res) => {
     res.render("index", { layout: "index" });
   });
@@ -21,6 +8,4 @@ module.exports = app => {
   app.get(`/api/recipes`, (req, res) => {
     res.render("viewall", { layout: "viewall" });
   });
-
-  // blog route loads blog.html
 };
