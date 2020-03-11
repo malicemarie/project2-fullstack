914b30a5b5fcccf381b1b2cdbfc7cb80f2d77a59
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-03-10 18:32:14 -0700

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,6 @@ USE recipes_db;
 CREATE TABLE recipes (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    amount INT NOT NULL,
-    measurement VARCHAR(30) NOT NULL,
     ingredientname VARCHAR(500) NOT NULL,
     category VARCHAR(50) NOT NULL,
     servingsize INT,
