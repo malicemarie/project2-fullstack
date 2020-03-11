96c4cce3a39f7d74109cf02432421f5c39417535
Spencer Fife <scottyfife17@gmail.com>
2020-03-11 15:55:48 -0700

    fixed html element, set up jquery functions

--- public/js/recipebox.js
+++ public/js/recipebox.js
@@ -1,3 +1,8 @@
 "use strict";
 
+//create recipe function
+$(document).ready(() => {});
+//view all recipe function
+$(document).ready(() => {});
+//view all categories function
 $(document).ready(() => {});
