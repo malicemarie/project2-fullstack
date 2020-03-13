5dde880ac518ae22a712d6385173dac7087377db
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-03-12 17:34:50 -0700

    on click event for adding a new recipe

--- public/js/recipebox.js
+++ public/js/recipebox.js
@@ -1,8 +1,42 @@
 "use strict";
 
-//create recipe function
-$(document).ready(() => {});
-//view all recipe function
-$(document).ready(() => {});
-//view all categories function
-$(document).ready(() => {});
+$(document).ready(() => {
+  //create/add new recipe
+  const titleInput = $("#title");
+  const ingredientsInput = $("#ingredients");
+  const categorySelect = $("#category");
+  const submitBtn = $("#submit-btn");
+
+  $(submitBtn).on("click", handleFormSubmit);
+  let recipesId;
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (
+      !titleInput.val().trim() ||
+      !ingredientsInput.val().trim() ||
+      !categorySelect.val()
+    ) {
+      return;
+    }
+  }
+  const newRecipe = {
+    title: titleInput.val().trim(),
+    ingredients: ingredientsInput.val().trim(),
+    category: categorySelect.val()
+  };
+
+  //updating recipe is not in our MVP but this if statement should come in handy later based on past activities
+  if (updating) {
+    newRecipe.id = recipeId;
+    updateRecipe(newRecipe);
+  } else {
+    submitRecipe(newRecipe);
+  }
+
+  function submitRecipe(recipe) {
+    $.post(`/api/recipes`, recipe, () => {
+      window.location.href = `/recipes`;
+    });
+  }
+});
