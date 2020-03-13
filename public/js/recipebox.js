ec3f9ce667ec54123928b7c43b8ff4c8b52ffa6d
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-03-12 18:44:51 -0700

    submit recipe and view all recipes ajax calls

--- public/js/recipebox.js
+++ public/js/recipebox.js
@@ -1,45 +1,64 @@
 "use strict";
 
-$(document).ready(() => {
-  //create/add new recipe
-  const titleInput = $("#title");
-  const ingredientsInput = $("#ingredients");
-  const categorySelect = $("#category");
-  const submitBtn = $("#submit-btn");
+//create/add new recipe
+const titleInput = $("#title");
+const ingredientsInput = $("#ingredients");
+const categorySelect = $("#category");
+const submitBtn = $("#submit-btn");
 
-  $(submitBtn).on("click", handleFormSubmit);
-  let recipesId;
+$(submitBtn).on("submit", handleFormSubmit);
+let recipesId;
+console.log("button click");
 
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (
-      !titleInput.val().trim() ||
-      !ingredientsInput.val().trim() ||
-      !categorySelect.val()
-    ) {
-      return;
-    }
+function handleFormSubmit(event) {
+  event.preventDefault();
+  if (
+    !titleInput.val().trim() ||
+    !ingredientsInput.val().trim() ||
+    !categorySelect.val()
+  ) {
+    return;
   }
-  const newRecipe = {
-    title: titleInput.val().trim(),
-    ingredients: ingredientsInput.val().trim(),
-    category: categorySelect.val()
-  };
+}
+const newRecipe = {
+  title: titleInput.val().trim(),
+  ingredients: ingredientsInput.val().trim(),
+  category: categorySelect.val()
+};
+console.log(newRecipe);
+//updating recipe is not in our MVP but this if statement should come in handy later based on past activities
+if (updating) {
+  newRecipe.id = recipeId;
+  updateRecipe(newRecipe);
+} else {
+  submitRecipe(newRecipe);
+}
 
-  //updating recipe is not in our MVP but this if statement should come in handy later based on past activities
-  if (updating) {
-    newRecipe.id = recipeId;
-    updateRecipe(newRecipe);
-  } else {
-    submitRecipe(newRecipe);
-  }
+function submitRecipe() {
+  $.ajax({
+    method: "POST",
+    url: "/api/recipes"
+  }).then(() => {
+    console.log("recipe Submitted successfully!");
+    window.location.href = `/viewall`;
+  });
+}
 
-  function submitRecipe() {
-    $.ajax({
-      method: "POST",
-      url: "/api/recipes"
-    }).then(() => {
-      window.location.href = `/viewall`;
-    });
-  }
-});
+//View all Recipes
+
+const viewAllBtn = $("#viewall-btn");
+
+$(submitBtn).on("submit", viewAllRecipes);
+let recipesId;
+console.log("button click");
+
+function viewAllRecipes(event) {
+  event.preventDefault();
+  $.ajax({
+    method: "GET",
+    url: "api/viewall"
+  }).then(() => {
+    console.log("view all recips success");
+    window.location.href = "/viewall";
+  });
+}
